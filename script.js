3daf9865720176550f6c87af208193ca69dbe27e
jamesc2000 <cruz.james99@gmail.com>
2020-03-20 11:24:09 +0800

    Added dropdown for education Added chevron svg
    TODO:
    Add dropdown animation
    Add dropdown for skillsets

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-var card = document.getElementsByClassName('educ-card');
-var collapsible = card.getElementById('collapsible');
+var uncollapse = document.getElementsByClassName('uncollapse-container');
+var uncollapseStatus
+var toggle = true;
 
-dropdown()
+function triggerToggle()
 {
-
+    if (toggle) {
+        uncollapseStatus = 'collapsed';
+        toggle = !toggle;
+    } else {
+        uncollapseStatus = 'uncollapsed';
+        toggle = !toggle;  
+    }    
 }
+
+// add event listeners to all uncollapse-containers
+for (let i = 0; i < uncollapse.length; i++) {
+    uncollapse[i].addEventListener("click", function() {
+        triggerToggle();
+        this.nextElementSibling.className = uncollapseStatus;
+    });
+}
+
