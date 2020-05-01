c3cceecdc18fbcfc00caf8376dad01a991954b05
jamesc2000 <cruz.james99@gmail.com>
2020-05-01 18:00:55 +0800

    + Fix attempt for qotd

--- script.js
+++ script.js
@@ -5,5 +5,9 @@ function media(x) {
 }
   
 var x = window.matchMedia("(max-width: 700px)")
-media(x) // Call listener function at run time
+media(x) // Call listener function at runtime
 x.addListener(media) // Attach listener function on state changes
+
+const quote = document.getElementById('quote')
+
+quote.innerHTML = TheySaidSo.render({ qod_category : 'inspire'});
